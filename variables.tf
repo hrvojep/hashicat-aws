816f4d155ff6e70851c3e66efde139f9e04f330f
H <hrvojep@yahoo.com>
2023-02-28 13:49:23 +1100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -26,7 +26,7 @@ variable "subnet_prefix" {
 
 variable "instance_type" {
   description = "Specifies the AWS instance type."
-  default     = "t2.small"
+  default     = "t2.xlarge"
 }
 
 variable "admin_username" {
