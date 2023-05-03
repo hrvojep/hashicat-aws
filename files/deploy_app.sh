bec25ff228263606b49cdc9efd665ef097ae1363
H <hrvojep@yahoo.com>
2023-05-03 15:01:41 +1000

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -2,6 +2,8 @@
 # Script to deploy a very simple web application.
 # The web app has a customizable image and some text.
 
+cp /home/ubuntu/CISRO-ALA.png /var/www/html/CISRO-ALA.png
+
 cat << EOM > /var/www/html/index.html
 <html>
   <head><title>Meow!</title></head>
@@ -9,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
-  <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
+  <center><img src="CISRO-ALA.png"></img></center>
   <center><h2>Meow World!</h2></center>
   Welcome to ${PREFIX}'s app.
   <!-- END -->
